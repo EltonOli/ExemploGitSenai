968d1d446e7f679318454fef64a33e02af009915
Elton de Oliveira <eltonfdeoliveira@gmail.com>
2022-01-10 03:20:08 -0400

    arquivo cadastroEvento.js adicionado

---
+++ cadastroEvento.js
@@ -0,0 +1,29 @@
+
+var dataAtual = [10/01/2022]
+let dataDoEvento = [12/01/2022]
+var nomePalestrante = ["Elton"]
+
+    if (dataDoEvento > dataAtual) {
+        console.log("Cadastrar Evento!")
+        console.log(nomePalestrante)
+    } 
+    else {
+        console.log("Data Inválida! Cadastro não permitido.")
+}
+
+var numeroParticipantes = [100]
+    if (numeroParticipantes < 100){
+        console.log("Digite o nome do participante:")
+    }
+    else {
+        console.log("Vagas esgotadas!")
+    }
+
+
+var idadeParticipante = [19]
+    if (idadeParticipante > 18){
+        console.log("Realizar Cadastro.")
+    }
+    else{
+        console.log("Idade não permitida para o evento!")
+    }
